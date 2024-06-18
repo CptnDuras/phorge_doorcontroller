3068671df0e55a4f5ae91af198a24cedfe2dba58
Steve Butler <steven.butler@gmail.com>
2024-06-18 16:50:36 -0600

    updating serial_mon to talk to ttyACM0 by default

--- backend/serial_mon.py
+++ backend/serial_mon.py
@@ -24,7 +24,7 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-def setup_serial_conn(port="/dev/ttyACM1"):
+def setup_serial_conn(port="/dev/ttyACM0"):
     ser = serial.Serial(port, 9600, timeout=2, xonxoff=False, rtscts=False,
                         dsrdtr=False)  # Tried with and without the last 3 parameters, and also at 1Mbps, same happens.
     ser.flushInput()
--- xiao/code.py
+++ xiao/code.py
@@ -1,3 +1,4 @@
 import read
 
 read.do_read()
+
